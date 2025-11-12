48e82fa673201b630d76cf168f28c1fdfac1c973
SANTIAGO MARTINEZ AYALA <smartineza_2@uqvirtual.edu.co>
2025-11-12 17:27:31 +0000

    mira el nombre como se sale de la ubicacion del banner , corrige esto pa

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -32,7 +32,7 @@ export function Header() {
     >
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
-          <Link href="/" className="text-2xl font-headline font-bold text-primary hover:text-accent transition-colors text-glow-primary">
+          <Link href="/" className="text-xl font-headline font-bold text-primary hover:text-accent transition-colors text-glow-primary">
             {t('about.name')}
           </Link>
 
